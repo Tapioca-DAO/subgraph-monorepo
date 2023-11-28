2c267b22a04f14e539d26f27253be087fbfa5410
0xshippor <14165372+0xshippor@users.noreply.github.com>
2023-11-28 12:53:00 +0100

    💾 chore: update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -10,6 +10,8 @@ module.exports = {
   ],
   plugins: ["unused-imports", "@typescript-eslint", "import", "@cspell"],
   rules: {
+    "unused-imports/no-unused-imports-ts": "error",
+    "import/no-duplicates": "error",
     "@typescript-eslint/consistent-type-imports": 0,
     "@typescript-eslint/no-unnecessary-type-assertion": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
@@ -23,6 +25,31 @@ module.exports = {
         },
       },
     ],
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        "types": {
+          // We need to allow BigInt because it is exported from "@graphprotocol/graph-ts"
+          "BigInt": false,
+        },
+        "extendDefaults": true
+      }
+    ],
+    // we need to allow empty constructors
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      {
+        "allow": [
+          "constructors"
+        ]
+      }
+    ],
+    "@typescript-eslint/no-inferrable-types": [
+      "error",
+      {
+        "ignoreParameters": true
+      }
+    ],
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -32,5 +59,8 @@ module.exports = {
     "/*.*", // ignore files in root
     "index.js", // this file
     "cspell-dictionary.js", // dictionary
+    "node_modules", 
+    "build", 
+    "generated"
   ],
 };
