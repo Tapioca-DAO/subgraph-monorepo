e645887f74ba4db19102c21de4a510f6a7ad04f3
0xshippor <14165372+0xshippor@users.noreply.github.com>
2024-01-29 14:47:54 +0100

    🔧 fix: update genesis addresses

--- subgraphs/genesis/src/constants.ts
+++ subgraphs/genesis/src/constants.ts
@@ -1,6 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 
 export const AOTAP_ADDRESS = "0x24268DA3FdB0Cd2ae216173748BC29Ad7684B9E1"
-export const VAULT_ADDRESS = "0x0000000000000000000000000000000000000000"
+export const VAULT_ADDRESS = "0xEce16053629a562474b9Ec2C89A2F25563400aB6"
 
 export const BASIS_POINT = BigInt.fromI32(10000 as i32)
