d1421442f16ebd179f3015678a3b9b89fb8205bd
0xshippor <14165372+0xshippor@users.noreply.github.com>
2024-05-09 20:01:11 +0200

    💾 chore: deployment scripts

--- subgraphs/genesis/deploy-prod.goldsky.sh
+++
@@ -1,5 +0,0 @@
-pnpm prepare:addresses -c 421614
-pnpm build --network arbitrum-sepolia --skip-migrations
-goldsky subgraph tag delete genesis-prod-421614/1.0.0 -t 421614
-goldsky subgraph delete genesis-prod-421614/1.0.0
-goldsky subgraph deploy genesis-prod-421614/1.0.0 --path . --start-block 41310428
--- subgraphs/genesis/deploy.goldsky.sh
+++
@@ -1,5 +0,0 @@
-pnpm prepare:addresses -c 421614
-pnpm build --network arbitrum-sepolia --skip-migrations
-goldsky subgraph tag delete genesis-dev-421614/1.0.0 -t 421614
-goldsky subgraph delete genesis-dev-421614/1.0.0
-goldsky subgraph deploy genesis-dev-421614/1.0.0 --path . --start-block 41310428
